b90e9803b68a3498701859d79151800fb32d2303
mevonuk <mevonuk@student.42.fr>
2026-06-30 14:55:57 +0200

    comments

--- tools/MLP_momentum.py
+++ tools/MLP_momentum.py
@@ -82,295 +82,7 @@ class MLP_momentum:
         self.rms_bh2 = np.zeros_like(self.bias_hidden2)
         self.rms_bo = np.zeros_like(self.bias_output)
 
-
-    def normalize_data(self, X, set_norm=False):
-        """normalize the dataset"""
-        features = X.columns
-        if set_norm or (self.mu is None) or (self.sigma is None):
-            # compute statistics on training set
-            self.mu, self.sigma = get_mean_std(X, features)
-
-        # apply normalization transformation to training set
-        X_scaled = X.copy()
-        X_scaled[features] = (X_scaled[features] - self.mu) / self.sigma
-        return X_scaled
-
-
-    def sigmoid(self, x):
-        """sigmoid function"""
-        x = np.clip(x, -500, 500)
-        return 1 / (1 + np.exp(-x))
-
-
-    def forward(self, X):
-        """forward propagation"""
-        # X is the input
-        # dot product of input with weights from input to hidden layer 1, add bias
-        self.hidden1_input = np.dot(X, self.weights_input_hidden1) + self.bias_hidden1
-        # activation function used on this...
-        self.hidden1_output = self.sigmoid(self.hidden1_input)
-        # then take hidden layer 1 output dotted with the wieghts from the hidden layer 1 to hidden layer 2 plus bias
-        self.hidden2_input = np.dot(self.hidden1_output, self.weights_hidden1_hidden2) + self.bias_hidden2
-        # activation function used on this...
-        self.hidden2_output = self.sigmoid(self.hidden2_input)
-        # then take sigmoid output dotted with the wieghts from the hidden layer to the output layer plus bias
-        self.final_input = np.dot(self.hidden2_output, self.weights_hidden2_output) + self.bias_output
-        # then use the sigmoid activation to get the output
-        self.final_output = self.sigmoid(self.final_input)
-        return self.final_output
-
-    def gd_grad(self, learning_rate, grad_wh2o, grad_bo,
-                grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
-        """does the backward propogation using the basic gradient descent method"""
-        # update the weights and biases from the second layer to the ouput layer
-        self.weights_hidden2_output -= learning_rate * grad_wh2o
-        self.bias_output -= learning_rate * grad_bo
-
-        # update the weights and bias between the two hidden layers
-        self.weights_hidden1_hidden2 -= learning_rate * grad_wh1h2
-        self.bias_hidden2 -= learning_rate * grad_bh2
-
-        # update the weights and bias for the input to the first hidden layer
-        self.weights_input_hidden1 -= learning_rate * grad_wih1
-        self.bias_hidden1 -= learning_rate * grad_bh1
-
-
-    def nesterov_update(self, param, grad, velocity, lr):
-        """Helper for the Nesterov momentum method"""
-        v_prev = velocity.copy()
-
-        velocity[:] = self.momentum * velocity - lr * grad
-
-        param += (
-            -self.momentum * v_prev
-            + (1 + self.momentum) * velocity
-        )
-
-    def nesterov_grad(self, learning_rate, grad_wh2o, grad_bo,
-                      grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
-        """does the backward propogation using the Nesterov momentum method"""
-        # update weights and biases
-        self.nesterov_update(
-            self.weights_hidden2_output,
-            grad_wh2o,
-            self.v_wh2o,
-            learning_rate
-        )
-
-        self.nesterov_update(
-            self.bias_output,
-            grad_bo,
-            self.v_bo,
-            learning_rate
-        )
-
-        self.nesterov_update(
-            self.weights_hidden1_hidden2,
-            grad_wh1h2,
-            self.v_wh1h2,
-            learning_rate
-        )
-
-        self.nesterov_update(
-            self.bias_hidden2,
-            grad_bh2,
-            self.v_bh2,
-            learning_rate
-        )
-
-        self.nesterov_update(
-            self.weights_input_hidden1,
-            grad_wih1,
-            self.v_wih1,
-            learning_rate
-        )
-
-        self.nesterov_update(
-            self.bias_hidden1,
-            grad_bh1,
-            self.v_bh1,
-            learning_rate
-        )
-
-    def adam_update(self, param, grad, m, v, lr):
-        """Adam optimizer update"""
-
-        # update biased moments
-        m[:] = self.beta1 * m + (1 - self.beta1) * grad
-        v[:] = self.beta2 * v + (1 - self.beta2) * (grad ** 2)
-
-        # bias correction
-        m_hat = m / (1 - self.beta1 ** self.t)
-        v_hat = v / (1 - self.beta2 ** self.t)
-
-        # parameter update
-        param -= lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
-
-
-    def adam_grad(self, learning_rate, grad_wh2o, grad_bo,
-                  grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
-        """does the backward propogation using the Adam optimizer"""
-
-        self.t += 1
-
-        # updates
-        self.adam_update(
-            self.weights_hidden2_output,
-            grad_wh2o,
-            self.m_wh2o,
-            self.s_wh2o,
-            learning_rate
-        )
-
-        self.adam_update(
-            self.bias_output,
-            grad_bo,
-            self.m_bo,
-            self.s_bo,
-            learning_rate
-        )
-
-        self.adam_update(
-            self.weights_hidden1_hidden2,
-            grad_wh1h2,
-            self.m_wh1h2,
-            self.s_wh1h2,
-            learning_rate
-        )
-
-        self.adam_update(
-            self.bias_hidden2,
-            grad_bh2,
-            self.m_bh2,
-            self.s_bh2,
-            learning_rate
-        )
-
-        self.adam_update(
-            self.weights_input_hidden1,
-            grad_wih1,
-            self.m_wih1,
-            self.s_wih1,
-            learning_rate
-        )
-
-        self.adam_update(
-            self.bias_hidden1,
-            grad_bh1,
-            self.m_bh1,
-            self.s_bh1,
-            learning_rate
-        )
-
-
-    def rmsprop_update(self, param, grad, cache, lr):
-        """RMSprop parameter update"""
-
-        cache[:] = (
-            self.rms_beta * cache
-            + (1 - self.rms_beta) * grad**2
-        )
-
-        param -= (
-            lr * grad
-            / (np.sqrt(cache) + self.epsilon)
-        )
-
-
-    def rmsprop_grad(self, learning_rate, grad_wh2o, grad_bo,
-                     grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
-        """does the backward propogation using RMSprop"""
-
-        self.rmsprop_update(
-            self.weights_hidden2_output,
-            grad_wh2o,
-            self.rms_wh2o,
-            learning_rate
-        )
-
-        self.rmsprop_update(
-            self.bias_output,
-            grad_bo,
-            self.rms_bo,
-            learning_rate
-        )
-
-        self.rmsprop_update(
-            self.weights_hidden1_hidden2,
-            grad_wh1h2,
-            self.rms_wh1h2,
-            learning_rate
-        )
-
-        self.rmsprop_update(
-            self.bias_hidden2,
-            grad_bh2,
-            self.rms_bh2,
-            learning_rate
-        )
-
-        self.rmsprop_update(
-            self.weights_input_hidden1,
-            grad_wih1,
-            self.rms_wih1,
-            learning_rate
-        )
-
-        self.rmsprop_update(
-            self.bias_hidden1,
-            grad_bh1,
-            self.rms_bh1,
-            learning_rate
-        )
-
-
-    def backward(self, X, y, output, learning_rate):
-        """backward propagation"""
-        # calculate the errors for each layer
-        output_error = output - y
-        hidden2_error = np.dot(output_error, self.weights_hidden2_output.T) * self.hidden2_output * (1 - self.hidden2_output)
-        hidden1_error = np.dot(hidden2_error, self.weights_hidden1_hidden2.T) * self.hidden1_output * (1 - self.hidden1_output)
-
-        m = X.shape[0]
-        # calculate gradients scaled to "batch size" m
-        grad_wh2o = np.dot(self.hidden2_output.T, output_error) / m
-        grad_bo = np.sum(output_error, axis=0, keepdims=True) / m
-
-        grad_wh1h2 = np.dot(self.hidden1_output.T, hidden2_error) / m
-        grad_bh2 = np.sum(hidden2_error, axis=0, keepdims=True) / m
-
-        grad_wih1 = np.dot(X.T, hidden1_error) / m
-        grad_bh1 = np.sum(hidden1_error, axis=0, keepdims=True) / m
-
-        # backpropagate according to optimizer type
-        if self.optimizer == 'gd':
-            self.gd_grad(
-                learning_rate,
-                grad_wh2o, grad_bo,
-                grad_wh1h2, grad_bh2,
-                grad_wih1, grad_bh1)
-
-        elif self.optimizer == 'nest':
-            self.nesterov_grad(
-                learning_rate,
-                grad_wh2o, grad_bo,
-                grad_wh1h2, grad_bh2,
-                grad_wih1, grad_bh1)
-
-        elif self.optimizer == 'adam':
-            self.adam_grad(
-                learning_rate,
-                grad_wh2o, grad_bo,
-                grad_wh1h2, grad_bh2,
-                grad_wih1, grad_bh1)
-
-        elif self.optimizer == 'rms':
-            self.rmsprop_grad(
-                learning_rate,
-                grad_wh2o, grad_bo,
-                grad_wh1h2, grad_bh2,
-                grad_wih1, grad_bh1)
-
+    # --- Training and prediction ---
 
     def train_with_validation(
             self, X_train, y_train,
@@ -470,6 +182,92 @@ class MLP_momentum:
         # return prediction and probability based on scaled data
         return self.make_prediction(X_scaled), self.forward(X_scaled)
 
+    # --- Modeling tools ---
+
+    def normalize_data(self, X, set_norm=False):
+        """normalize the dataset"""
+        features = X.columns
+        if set_norm or (self.mu is None) or (self.sigma is None):
+            # compute statistics on training set
+            self.mu, self.sigma = get_mean_std(X, features)
+
+        # apply normalization transformation to training set
+        X_scaled = X.copy()
+        X_scaled[features] = (X_scaled[features] - self.mu) / self.sigma
+        return X_scaled
+
+
+    def sigmoid(self, x):
+        """sigmoid function"""
+        x = np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-x))
+
+
+    def forward(self, X):
+        """forward propagation"""
+        # X is the input
+        # dot product of input with weights from input to hidden layer 1, add bias
+        self.hidden1_input = np.dot(X, self.weights_input_hidden1) + self.bias_hidden1
+        # activation function used on this...
+        self.hidden1_output = self.sigmoid(self.hidden1_input)
+        # then take hidden layer 1 output dotted with the wieghts from the hidden layer 1 to hidden layer 2 plus bias
+        self.hidden2_input = np.dot(self.hidden1_output, self.weights_hidden1_hidden2) + self.bias_hidden2
+        # activation function used on this...
+        self.hidden2_output = self.sigmoid(self.hidden2_input)
+        # then take sigmoid output dotted with the wieghts from the hidden layer to the output layer plus bias
+        self.final_input = np.dot(self.hidden2_output, self.weights_hidden2_output) + self.bias_output
+        # then use the sigmoid activation to get the output
+        self.final_output = self.sigmoid(self.final_input)
+        return self.final_output
+
+    def backward(self, X, y, output, learning_rate):
+        """backward propagation"""
+        # calculate the errors for each layer
+        output_error = output - y
+        hidden2_error = np.dot(output_error, self.weights_hidden2_output.T) * self.hidden2_output * (1 - self.hidden2_output)
+        hidden1_error = np.dot(hidden2_error, self.weights_hidden1_hidden2.T) * self.hidden1_output * (1 - self.hidden1_output)
+
+        m = X.shape[0]
+        # calculate gradients scaled to "batch size" m
+        grad_wh2o = np.dot(self.hidden2_output.T, output_error) / m
+        grad_bo = np.sum(output_error, axis=0, keepdims=True) / m
+
+        grad_wh1h2 = np.dot(self.hidden1_output.T, hidden2_error) / m
+        grad_bh2 = np.sum(hidden2_error, axis=0, keepdims=True) / m
+
+        grad_wih1 = np.dot(X.T, hidden1_error) / m
+        grad_bh1 = np.sum(hidden1_error, axis=0, keepdims=True) / m
+
+        # backpropagate according to optimizer type
+        if self.optimizer == 'gd':
+            self.gd_grad(
+                learning_rate,
+                grad_wh2o, grad_bo,
+                grad_wh1h2, grad_bh2,
+                grad_wih1, grad_bh1)
+
+        elif self.optimizer == 'nest':
+            self.nesterov_grad(
+                learning_rate,
+                grad_wh2o, grad_bo,
+                grad_wh1h2, grad_bh2,
+                grad_wih1, grad_bh1)
+
+        elif self.optimizer == 'adam':
+            self.adam_grad(
+                learning_rate,
+                grad_wh2o, grad_bo,
+                grad_wh1h2, grad_bh2,
+                grad_wih1, grad_bh1)
+
+        elif self.optimizer == 'rms':
+            self.rmsprop_grad(
+                learning_rate,
+                grad_wh2o, grad_bo,
+                grad_wh1h2, grad_bh2,
+                grad_wih1, grad_bh1)
+
+    # --- Load and save operations ---
 
     def save_weights(self, file_name='trained_weights_'):
         """Save weights, biases, norm factors to a pickle file"""
@@ -516,3 +314,214 @@ class MLP_momentum:
         self.bias_output = model_data['bias_output']
         self.mu = model_data['mu']
         self.sigma = model_data['sigma']
+
+    # --- Optimizers ---
+
+    # --- Standard gradient descent ---
+
+    def gd_grad(self, learning_rate, grad_wh2o, grad_bo,
+                grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
+        """does the backward propogation using the basic gradient descent method"""
+        # update the weights and biases from the second layer to the ouput layer
+        self.weights_hidden2_output -= learning_rate * grad_wh2o
+        self.bias_output -= learning_rate * grad_bo
+
+        # update the weights and bias between the two hidden layers
+        self.weights_hidden1_hidden2 -= learning_rate * grad_wh1h2
+        self.bias_hidden2 -= learning_rate * grad_bh2
+
+        # update the weights and bias for the input to the first hidden layer
+        self.weights_input_hidden1 -= learning_rate * grad_wih1
+        self.bias_hidden1 -= learning_rate * grad_bh1
+
+    # --- Nesterov momentum ---
+
+    def nesterov_update(self, param, grad, velocity, lr):
+        """Helper for the Nesterov momentum method"""
+        v_prev = velocity.copy()
+
+        velocity[:] = self.momentum * velocity - lr * grad
+
+        param += (
+            -self.momentum * v_prev
+            + (1 + self.momentum) * velocity
+        )
+
+    def nesterov_grad(self, learning_rate, grad_wh2o, grad_bo,
+                      grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
+        """does the backward propogation using the Nesterov momentum method"""
+        # update weights and biases
+        self.nesterov_update(
+            self.weights_hidden2_output,
+            grad_wh2o,
+            self.v_wh2o,
+            learning_rate
+        )
+
+        self.nesterov_update(
+            self.bias_output,
+            grad_bo,
+            self.v_bo,
+            learning_rate
+        )
+
+        self.nesterov_update(
+            self.weights_hidden1_hidden2,
+            grad_wh1h2,
+            self.v_wh1h2,
+            learning_rate
+        )
+
+        self.nesterov_update(
+            self.bias_hidden2,
+            grad_bh2,
+            self.v_bh2,
+            learning_rate
+        )
+
+        self.nesterov_update(
+            self.weights_input_hidden1,
+            grad_wih1,
+            self.v_wih1,
+            learning_rate
+        )
+
+        self.nesterov_update(
+            self.bias_hidden1,
+            grad_bh1,
+            self.v_bh1,
+            learning_rate
+        )
+    
+    # --- Adam optimizer ---
+
+    def adam_update(self, param, grad, m, v, lr):
+        """Adam optimizer update"""
+
+        # update biased moments
+        m[:] = self.beta1 * m + (1 - self.beta1) * grad
+        v[:] = self.beta2 * v + (1 - self.beta2) * (grad ** 2)
+
+        # bias correction
+        m_hat = m / (1 - self.beta1 ** self.t)
+        v_hat = v / (1 - self.beta2 ** self.t)
+
+        # parameter update
+        param -= lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
+
+
+    def adam_grad(self, learning_rate, grad_wh2o, grad_bo,
+                  grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
+        """does the backward propogation using the Adam optimizer"""
+
+        self.t += 1
+
+        # updates
+        self.adam_update(
+            self.weights_hidden2_output,
+            grad_wh2o,
+            self.m_wh2o,
+            self.s_wh2o,
+            learning_rate
+        )
+
+        self.adam_update(
+            self.bias_output,
+            grad_bo,
+            self.m_bo,
+            self.s_bo,
+            learning_rate
+        )
+
+        self.adam_update(
+            self.weights_hidden1_hidden2,
+            grad_wh1h2,
+            self.m_wh1h2,
+            self.s_wh1h2,
+            learning_rate
+        )
+
+        self.adam_update(
+            self.bias_hidden2,
+            grad_bh2,
+            self.m_bh2,
+            self.s_bh2,
+            learning_rate
+        )
+
+        self.adam_update(
+            self.weights_input_hidden1,
+            grad_wih1,
+            self.m_wih1,
+            self.s_wih1,
+            learning_rate
+        )
+
+        self.adam_update(
+            self.bias_hidden1,
+            grad_bh1,
+            self.m_bh1,
+            self.s_bh1,
+            learning_rate
+        )
+
+    # --- RMSprop ---
+
+    def rmsprop_update(self, param, grad, cache, lr):
+        """RMSprop parameter update"""
+
+        cache[:] = (
+            self.rms_beta * cache
+            + (1 - self.rms_beta) * grad**2
+        )
+
+        param -= (
+            lr * grad
+            / (np.sqrt(cache) + self.epsilon)
+        )
+
+    def rmsprop_grad(self, learning_rate, grad_wh2o, grad_bo,
+                     grad_wh1h2, grad_bh2, grad_wih1, grad_bh1):
+        """does the backward propogation using RMSprop"""
+
+        self.rmsprop_update(
+            self.weights_hidden2_output,
+            grad_wh2o,
+            self.rms_wh2o,
+            learning_rate
+        )
+
+        self.rmsprop_update(
+            self.bias_output,
+            grad_bo,
+            self.rms_bo,
+            learning_rate
+        )
+
+        self.rmsprop_update(
+            self.weights_hidden1_hidden2,
+            grad_wh1h2,
+            self.rms_wh1h2,
+            learning_rate
+        )
+
+        self.rmsprop_update(
+            self.bias_hidden2,
+            grad_bh2,
+            self.rms_bh2,
+            learning_rate
+        )
+
+        self.rmsprop_update(
+            self.weights_input_hidden1,
+            grad_wih1,
+            self.rms_wih1,
+            learning_rate
+        )
+
+        self.rmsprop_update(
+            self.bias_hidden1,
+            grad_bh1,
+            self.rms_bh1,
+            learning_rate
+        )
