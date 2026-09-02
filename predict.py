fix to softmax metrics

predict.py
@@ -1,6 +1,7 @@
 from tools.load_save_data import load_split_data
 from tools.math_tools import my_metrics, binary_cross_entropy
 from tools.MLP import MLP
+import numpy as np
 
 
 def predict(optimizer, X, y, activation, verbose):
@@ -17,8 +18,11 @@ def predict(optimizer, X, y, activation, verbose):
         loss = binary_cross_entropy(y.to_numpy(), output_prob)
 
         # compare prediction to real values
+        if activation == 'softmax':
+            y_val = np.argmax(y.to_numpy(), axis=1)
+            output_pred = np.argmax(output_prediction, axis=1)
         accuracy, precision, recall, F1 = my_metrics(
-            output_prediction, y.to_numpy())
+            output_pred, y_val)
 
         return accuracy, precision, recall, F1, loss
 

tools/MLP.py
@@ -134,8 +134,20 @@ class MLP:
             out_train = self.make_prediction(X_train)
             out_val = self.make_prediction(X_val)
 
-            t_acc, t_per, t_recall, t_F1 = my_metrics(out_train, y_train)
-            v_acc, v_per, v_recall, v_F1 = my_metrics(out_val, y_val)
+            if self.activation == 'softmax':
+                y_t = np.argmax(y_train, axis=1)
+                out_t = np.argmax(out_train, axis=1)
+            else:
+                y_t = y_train
+                out_t = out_train
+            t_acc, t_per, t_recall, t_F1 = my_metrics(out_t, y_t)
+            if self.activation == 'softmax':
+                y_v = np.argmax(y_val, axis=1)
+                out_v = np.argmax(out_val, axis=1)
+            else:
+                y_v = y_val
+                out_v = out_val
+            v_acc, v_per, v_recall, v_F1 = my_metrics(out_v, y_v)
 
             training_precision.append(t_per)
             training_F1.append(t_F1)